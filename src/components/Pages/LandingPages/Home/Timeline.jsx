fcc9714028ee64140d6605edc49a2ad640452aa5
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-04-10 13:08:45 +0600

    Updated

--- src/components/Pages/LandingPages/Home/Timeline.jsx
+++ src/components/Pages/LandingPages/Home/Timeline.jsx
@@ -2,16 +2,26 @@
 
 import Image from "next/image";
 import steps from "@/assets/images/steps.png";
+import timeline from "@/assets/images/timeline.png";
 
 const Timeline = () => {
   return (
     <section className="relative w-full py-10 lg:py-32 bg-primaryLight mt-20 lg:mt-32 overflow-hidden">
       <div className="my-container relative z-10">
-        <h1 className="text-center text-3xl lg:text-6xl font-medium mb-10 lg:mb-20 font-bricolage">
+        <h1 className="text-center text-3xl lg:text-6xl font-medium mb-5 lg:mb-20 font-bricolage">
           Website Setup{" "}
           <span className="font-semibold text-primary">Process</span>
         </h1>
-        <Image src={steps} alt="steps" className="w-full" />
+        <Image
+          src={steps}
+          alt="steps"
+          className="w-[1200px] h-fit mx-auto hidden lg:block"
+        />
+        <Image
+          src={timeline}
+          alt="timeline"
+          className="w-full h-fit lg:hidden"
+        />
       </div>
     </section>
   );
